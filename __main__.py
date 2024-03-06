0acb4713dfb63bcda0b8e180a8ed15f1296fc087
DemoDevv <mathieulebras95@gmail.com>
2024-03-06 10:45:07 +0100

    modification de la structure du projet pour prendre en compte les update chaque mois (WIP)

--- __main__.py
+++ __main__.py
@@ -4,14 +4,21 @@ from pandas import read_excel, NA
 import argparse
 
 
+quadrimestres = [[1, 2, 3, 4], [5, 6, 7, 8], [9, 10, 11, 12]]
+
+
 def sanitize(clients_total):
     # enlever les colonnes inutiles qui vont compliqué le collage des données sur la liste sharepoint
-    return clients_total.drop(columns=["Type d'élément", "Chemin d'accès"])
+    if "Type d'élément" in clients_total.columns:
+        clients_total = clients_total.drop(columns=["Type d'élément"])
+    if "Chemin d'accès" in clients_total.columns:
+        clients_total = clients_total.drop(columns=["Chemin d'accès"])
+    return clients_total
 
 
-def create_output_file(clients_total, new: bool):
+def create_output_file(clients_total, args):
     clients_total_sanitized = sanitize(clients_total)
-    if new:
+    if args.output:
         clients_total_sanitized.to_excel(args.output, index=False)
     else:
         clients_total_sanitized.to_excel("Clients_echantillons.xlsx", index=False)
@@ -19,51 +26,26 @@ def create_output_file(clients_total, new: bool):
     print("Fichier excel créé avec succès au chemin: ", args.output if args.output else "Clients_echantillons.xlsx")
 
 
-def merge_small_population(clients_total, population, fusion_len):
-    assert fusion_len > 0, "La taille de fusion doit être supérieure à 0"
-
-    # Filtrer les populations qui ont moins de 8 clients
-    small_groups = population.groupby("Code DT").filter(lambda x: len(x) < fusion_len)
-
-    # transformer la liste en string avec un tiret entre chaque code DT
-    merged_name_code_DT = "-".join(small_groups["Code DT"].unique())
-    
-    # Fusionner les petits groupes avec le reste du groupe
-    clients_total.loc[small_groups.index, "Code DT"] = merged_name_code_DT
+def update(clients_total, args):
+    pass
 
 
-def main(args):
-    # lire le fichier excel
-    clients_total = read_excel(args.path)
-
-    # remplacer les bonnes valeurs NA par _NA pour que pandas les prenne en compte
-    clients_total["PERIMETRE  DTO - DirInnov"] = clients_total["PERIMETRE  DTO - DirInnov"].fillna("Nouvelle Aquitaine")
-
-    # enlever les rows ou "Direction DTO ou Innov" est vide
-    clients_total = clients_total[clients_total["Direction DTO ou Innov"].isna() == False]
-
+def initialisation(clients_total, args):
     clients_total_size = clients_total.shape[0]
 
-    # ajout d'une colonne pour ensuite insérer une valeur entre 1 et 4 pour chaque client
-    clients_total["echantillon"] = NA
-
-    # regrouper les clients par population differente
-    clients_by_population = clients_total.groupby(["Direction DTO ou Innov",
-                                                   "PERIMETRE  DTO - DirInnov"])
-
-    # fusionner les populations qui ont moins de 8 clients
-    for _, population in clients_by_population:
-        merge_small_population(clients_total, population, args.fusion)
+    clients_total["Echantillon"] = NA
 
     # regrouper les clients par population differente après la fusion
     clients_by_population = clients_total.groupby(["Direction DTO ou Innov",
                                                    "PERIMETRE  DTO - DirInnov",
-                                                   "Code DT"])
+                                                   "Profil Client"])
 
     total_client = 0
         
     # print la table par population
-    for _, clients in clients_by_population:
+    for name, clients in clients_by_population:
+
+        print(name)
 
         size_population_total = clients.shape[0]
 
@@ -74,56 +56,85 @@ def main(args):
 
         # séparer la population en 4 échantillons de 25%
         for i in range(1, 5):
-            clients_not_in_echantillon = clients[clients['echantillon'].isna() == True]
+            clients_not_in_echantillon = clients[clients['Echantillon'].isna() == True]
 
             # prendre les 25% de la population
             head_25 = clients_not_in_echantillon.head(size_25)
 
             # remplir la colonne echantillon
-            clients.loc[head_25.index, "echantillon"] = i
+            clients.loc[head_25.index, "Echantillon"] = i
 
         # ajouter au reste des clients un échantillon unique
-        clients_not_in_echantillon_reste = clients[clients['echantillon'].isna() == True]
+        clients_not_in_echantillon_reste = clients[clients['Echantillon'].isna() == True]
 
         echantillon = [1, 2, 3, 4]
         shuffle(echantillon)
 
         # ajouter le reste des clients a un échantillon
         for client in clients_not_in_echantillon_reste.index:
-            clients.loc[client, "echantillon"] = echantillon.pop(0)
+            clients.loc[client, "Echantillon"] = echantillon.pop(0)
 
         # ajouter le nombre de clients a la population totale
         total_client += clients.shape[0]
 
         # associer le échantillons a la population total
-        clients_total.loc[clients.index, "echantillon"] = clients["echantillon"]
+        clients_total.loc[clients.index, "Echantillon"] = clients["Echantillon"]
 
-    assert clients_total[clients_total['echantillon'].isna() == True].shape[0] == 0, "Certaines entrées ne\
+    assert clients_total[clients_total['Echantillon'].isna() == True].shape[0] == 0, "Certaines entrées ne\
           sont affectées à aucun échantillons"
     assert total_client == clients_total_size, "Le résultats des échantillons ne contient pas autant d'entrées\
           que au début du programme"
 
-    print(clients_total.groupby("echantillon").size())
+    print(clients_total.groupby("Echantillon").size())
 
     # créer un nouveau fichier excel en sortie
-    create_output_file(clients_total, args.output)
+    create_output_file(clients_total, args)
+
+
+def main(args):
+    # lire le fichier excel
+    clients_total = read_excel(args.path)
+
+    # ajout d'une colonne pour ensuite insérer une valeur entre 1 et 4 pour chaque client
+    if "Echantillon" not in clients_total.columns:
+        clients_total["Echantillon"] = NA
+
+    if args.update:
+        update(clients_total, args)
+    else:
+        initialisation(clients_total, args)
+
+    # exemple pour obtenir le mois
+    # # convertir la colonne "Créé" en datetime pour en obtenir le mois
+    # mois = clients_total.head(1)["Créé"].dt.year[0]
+    # print(mois)
+
+    # remplacer les bonnes valeurs NA par _NA pour que pandas les prenne en compte
+    # clients_total["PERIMETRE  DTO - DirInnov"] = clients_total["PERIMETRE  DTO - DirInnov"].fillna("Nouvelle Aquitaine")
+    # TODO: modifier directement les valeurs NA dans la liste sharepoint
+
+    # enlever les rows ou "Direction DTO ou Innov" est vide
+    # clients_total = clients_total[clients_total["Direction DTO ou Innov"].isna() == False]
 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Echantillonage des clients")
-    parser.add_argument("--path",
+    parser.add_argument(
+        "-p",
+        "--path",
         help="Chemin du fichier excel",
         type=str, 
         required=True
     )
     parser.add_argument(
-        "--fusion",
-        help="Nombre de clients minimum pour fusionner les populations",
-        type=int,
-        default=10,
-        required=False,
+        "-u",
+        "--update",
+        help="Indique si le fichier excel doit être mis à jour",
+        action="store_true",
     )
-    parser.add_argument("--output",
+    parser.add_argument(
+        "-o",
+        "--output",
         help="Chemin du fichier de sortie",
         type=str,
         required=False
